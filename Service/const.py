6a157fb0bfaef0b74c772fb715b26cf14c9ed2b1
Худяков Кирилл Константинович <Khudyakov.K.Ko@sberbank.ru>
2020-09-23 18:10:26 +0300

    Теперь конфигурация и секреты лежат не в индексе гита

--- Service/const.py
+++ Service/const.py
@@ -1,17 +1,26 @@
 from random import randint
-url_send_message = """https://api.telegram.org/bot1238618041:AAGiAHI9FyQHMM4IpYqS_YRTMTxCpDnODJM/sendMessage"""
+from json import load
+
+with open('Cache/files/cnfg.json') as json_file:
+    data = load(json_file)
+
+url_fasttext = data['url_fasttext']
+token_fastext = data['token_fastext']
+server_memcached = data['server_memcached']
+bot_token = data['bot_token']
+print(url_fasttext)
+print(token_fastext)
+print(server_memcached)
+print(bot_token)
+
+url_send_message = """https://api.telegram.org/bot{}/sendMessage""".format(bot_token)
 index_filename = 'Cache/files/index_cache.p'
 vacs_filename = 'Cache/files/struct_actual_vac.pickle'
 vacancy_url = "https://my.sbertalents.ru/#/job-requisition/{}"
 
 # константы работы движка
 
-url_fasttext = "http://104.154.103.236:8080/FastTextAsServer"
-token_fastext = "dkhfklsdhflksdhflksdhf43934"
-
-server_memcached = "127.0.0.1:11211"
-
-time_for_update_index = 60*60*1
+time_for_update_index = 60*60*6
 
 time_for_sleep_sheduller = 60*60*12
 
@@ -20,3 +29,8 @@ timeout_for_chat = 300
 timeout_for_chat_test = 5
 
 unique_quid_app = randint(0, 10000)
+
+
+
+
+
